e7f818ec0dd2938a24c152fe53bc38273b3a6d2f
Charles Archibong <info@charlesarchibong.copm>
2022-02-03 07:47:36 +0100

    added a create request

--- smart_contract/contracts/Campaign.sol
+++ smart_contract/contracts/Campaign.sol
@@ -3,9 +3,30 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Campaign {
+    struct Request {
+        string description;
+        address recipient;
+        uint256 value;
+        bool complete;
+    }
+
     address public manager;
     uint256 public minimumContribution;
     address[] public approvers;
+    Request[] public requests;
+
+    modifier minimum() {
+        require(
+            msg.value >= minimumContribution,
+            "Contributed amount is less than minimum amount required"
+        );
+        _;
+    }
+
+    modifier managerOnly() {
+        require(msg.sender == manager, "Only manager can perform this action");
+        _;
+    }
 
     constructor(uint256 minimumAmount) {
         manager = msg.sender;
@@ -16,11 +37,18 @@ contract Campaign {
         approvers.push(msg.sender);
     }
 
-    modifier minimum() {
-        require(
-            msg.value > minimumContribution,
-            "Contributed amount is less than minimum amount required"
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address recipient
+    ) public managerOnly {
+        requests.push(
+            Request({
+                description: description,
+                recipient: recipient,
+                value: value,
+                complete: false
+            })
         );
-        _;
     }
 }
