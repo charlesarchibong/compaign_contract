e11e8b88491b63ad633dec88917609570070d356
Charles Archibong <info@charlesarchibong.copm>
2022-03-08 03:44:39 +0100

    finakize contract and developed new contract factory so we can having multiple contract for each campaign and users are sorely incharge

--- smart_contract/contracts/Campaign.sol
+++ smart_contract/contracts/Campaign.sol
@@ -2,18 +2,35 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint256 minimusAmount) public payable {
+        Campaign campaign = new Campaign(minimusAmount, msg.sender);
+        deployedCampaigns.push(address(campaign));
+    }
+
+    function getDeployedCampaigns() public view returns (address[] memory) {
+        return deployedCampaigns;
+    }
+}
+
 contract Campaign {
     struct Request {
         string description;
         address recipient;
         uint256 value;
         bool complete;
+        uint256 noOfApprovals;
+        mapping(address => bool) approvals;
     }
 
     address public manager;
     uint256 public minimumContribution;
-    address[] public approvers;
+
+    mapping(address => bool) public approvers;
     Request[] public requests;
+    uint256 public approversCount;
 
     modifier minimum() {
         require(
@@ -28,13 +45,14 @@ contract Campaign {
         _;
     }
 
-    constructor(uint256 minimumAmount) {
-        manager = msg.sender;
+    constructor(uint256 minimumAmount, address creator) {
+        manager = creator;
         minimumContribution = minimumAmount;
     }
 
     function contribute() public payable minimum {
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(
@@ -42,13 +60,33 @@ contract Campaign {
         uint256 value,
         address recipient
     ) public managerOnly {
-        requests.push(
-            Request({
-                description: description,
-                recipient: recipient,
-                value: value,
-                complete: false
-            })
+        Request storage newRequest = requests.push();
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.description = description;
+        newRequest.complete = false;
+        newRequest.noOfApprovals = 0;
+    }
+
+    function approveReques(uint256 index) public {
+        Request storage request = requests[index];
+        require(
+            approvers[msg.sender],
+            "You must contribute to the campaign before you can vote"
         );
+        require(!request.approvals[msg.sender], "You can only vote once");
+        request.approvals[msg.sender] = true;
+        request.noOfApprovals++;
+    }
+
+    function finalizeRequest(uint256 index) public payable managerOnly {
+        Request storage request = requests[index];
+        require(request.complete == false, "Request is already finalized");
+        require(
+            request.noOfApprovals > approversCount / 2,
+            "Not enough approvals, request must have 50% approvers before it can be finalized"
+        );
+        payable(request.recipient).transfer(request.value);
+        request.complete = true;
     }
 }
